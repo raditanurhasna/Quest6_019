829ee0397244551d3e067864f75410cbc93cd582
raditanurhasna <raditanurhasna1410@gmail.com>
2024-11-22 17:35:08 +0700

    Menambahkan navigasi ke MahasiswaFormView dengan aksi submit dan kembali.

--- app/src/main/java/com/example/navigasicompose/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigasicompose/navigation/PengelolaHalaman.kt
@@ -11,7 +11,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.navigasicompose.ui.theme.view.screen.SplashView
-import com.example.navigasicompose.ui.theme.view.viewmodel.MahasiswaViewModel
 import com.example.navigasicompose.ui.theme.view.viewmodel.RencanaStudyViewModel
 import com.example.navigasicompose.ui.view.viewmodel.MahasiswaViewModel
 import com.example.project.ui.view.screen.MahasiswaFormView
@@ -29,7 +28,32 @@ fun MahasiswaApp(
     mahasiswaViewModel: MahasiswaViewModel = viewModel(),
     krsViewModel: RencanaStudyViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
-) {}
+){
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUIState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ){
+        composable(route = Halaman.Splash.name){
+            SplashView(onMulaiButton = {
+                navController.navigate(
+                    Halaman.Mahasiswa.name
+                )
+            })
+        }
+        composable(route = Halaman.Mahasiswa.name) {
+            MahasiswaFormView(
+                onSubmitButtonClicked = {
+                    mahasiswaViewModel.saveDataMahasiswa(it)
+                    navController.navigate(Halaman.Matakuliah.name)
+                },
+                onBackButtonClicked = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
 
 
 
