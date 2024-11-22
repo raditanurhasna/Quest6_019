09eb02cb4aa9b2ce9ce6dfa87a10b027296419df
raditanurhasna <raditanurhasna1410@gmail.com>
2024-11-22 16:57:16 +0700

    Memperbaiki MahasiswaViewModel.

--- app/src/main/java/com/example/navigasicompose/ui/theme/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/navigasicompose/ui/theme/view/viewmodel/MahasiswaViewModel.kt
@@ -1,22 +1,20 @@
-package com.example.navigasicompose.ui.theme.view.viewmodel
+package com.example.navigasicompose.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.navigasicompose.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel : ViewModel() {
-    private val _dataModel = MutableStateFlow(Mahasiswa())
-    val dataModel: StateFlow<Mahasiswa> = _dataModel.asStateFlow()
 
-    fun saveDataMhs(listData: MutableList<String>){
-        _dataModel.update { mhs -> mhs.copy(
-            nim = listData[0],
-            nama = listData[1],
-            email = listData[2]
-        ) }
+    private val _mahasiswaStateUI = MutableStateFlow(Mahasiswa())
+    val mahasiswaUIState: StateFlow<Mahasiswa> = _mahasiswaStateUI.asStateFlow()
+    fun saveDataMahasiswa(ls: MutableList<String>){
+        _mahasiswaStateUI.value = Mahasiswa(
+            nim = ls[0],
+            nama = ls[1],
+            email = ls[2]
+        )
     }
 }
-
