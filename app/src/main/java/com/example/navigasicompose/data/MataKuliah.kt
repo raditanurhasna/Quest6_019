e3a6d2869461e0fac324d7b0286a18842bf2f15b
raditanurhasna <raditanurhasna1410@gmail.com>
2024-11-22 09:14:05 +0700

    Menambahkan daftar opsi mata kuliah ke dalam objek MataKuliah.

---
+++ app/src/main/java/com/example/navigasicompose/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.navigasicompose.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemprograman Aplikasi Mobile Lanjut",
+        "Pemprograman Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+}
